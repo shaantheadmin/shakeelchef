de93badf5da550a0d9e2bea5fa2069d333be4c52
git/shaantheadmin <git/shaantheadmin@gmail.com>
2012-02-28 16:10:24 +0000

    Your commit message6

--- cookbooks/shakeelcookbook/metadata.rb
+++ cookbooks/shakeelcookbook/metadata.rb
@@ -6,3 +6,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "0.0.1"
 recipe "shakeelchef::default", "Prints a message to the output"
 depends "rs_utils"
+attribute "shakeelcookbook/custom_input",
+:display_name => "A Custom Chef Input",
+:choice => ["A", "B", "C"],
+:recipes => ["shakeelcookbook::default"]
--- cookbooks/shakeelcookbook/recipes/default.rb
+++ cookbooks/shakeelcookbook/recipes/default.rb
@@ -8,4 +8,12 @@
 #
 rs_utils_marker :begin
 log "your message"
+package_name = value_for_platform(
+["centos", "redhat", "suse", "fedora" ] => { "default" => "httpd" },
+["ubuntu", "debian"] => { "default" => "apache2" }
+)
+package package_name do
+  action :install
+  end
+  log "The package that will be installed is #{package_name}"
 rs_utils_marker :end
